d0fa2367001bf8839c5b6c9d26f24b180d028e20
Mohammad Salman <mohammad.salman2696@gmail.com>
2024-08-01 12:27:33 +0530

    chart changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Doughnut, Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import React from "react";
+import PropTypes from "prop-types";
+import { Doughnut, Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
 
-const Card = ({ icon, title, value, description, percentageChange = '', period, type, data }) => {
+const Card = ({
+  icon,
+  title,
+  value,
+  description,
+  percentageChange = "",
+  period,
+  type,
+  data,
+}) => {
   const renderChart = () => {
     switch (type) {
-      case 'circular':
-        return <Doughnut data={data} options={{ 
-          rotation: 270, 
-          circumference: 180, 
-          cutout: '80%' // Adjust this to control the thickness
-        }}  />;
-      case 'bar':
+      case "circular":
+        return (
+          <div className="h-48">
+          <Doughnut
+            data={data}
+            options={{
+              rotation: 270,
+              circumference: 180,
+              cutout: "80%", // Adjust this to control the thickness
+            }}
+          />
+          </div>
+        );
+      case "bar":
         return <Bar data={data} />;
       default:
         return null;
@@ -26,9 +59,7 @@ const Card = ({ icon, title, value, description, percentageChange = '', period, 
       {icon}
       <p className="card-title text-sm font-bold mt-4">{title}</p>
       {type ? (
-        <div className="mt-4">
-          {renderChart()}
-        </div>
+        <div className="mt-4">{renderChart()}</div>
       ) : (
         <div className="flex flex-col gap-1 my-2">
           <h1 className="text-2xl font-bold text-gray-800">{value}</h1>
@@ -36,7 +67,11 @@ const Card = ({ icon, title, value, description, percentageChange = '', period, 
         </div>
       )}
       <div className="flex flex-row gap-1 mt-5 text-sm">
-        <div className={`text-green-500 ${percentageChange.startsWith('-') ? 'text-red-500' : ''}`}>
+        <div
+          className={`text-green-500 ${
+            percentageChange.startsWith("-") ? "text-red-500" : ""
+          }`}
+        >
           {percentageChange}
         </div>
         <div className="stat-title">{period}</div>
