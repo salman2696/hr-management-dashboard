019a131d19672245bb9b2b748111afc55c684f53
Mohammad Salman <mohammad.salman2696@gmail.com>
2024-08-09 11:40:37 +0530

    changes saved

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import React, { useContext } from "react";
-import Sidebar from './Sidebar'; // Adjust the path according to your file structure
-import Header from './Header'; // Adjust the path according to your file structure
-import { ThemeContext } from './ThemeContext';
+import Sidebar from "./Sidebar"; // Adjust the path according to your file structure
+import Header from "./Header"; // Adjust the path according to your file structure
+import { ThemeContext } from "./ThemeContext";
 
 const Layout = () => {
-    const { theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
   return (
     <>
       <Sidebar />
-        <Header />
-        <main className={`dashboard-page lg:ml-[20%] p-4 mt-16 ${
-        theme === "dark"
-          ? "bg-gray-800 text-gray-300"
-          : "bg-[#f6f7f9] text-black"
-      }`}>
-          <Outlet />
-        </main>
+      <Header />
+      <main
+        className={`dashboard-page lg:ml-[20%] p-4 mt-16 ${
+          theme === "dark"
+            ? "bg-gray-800 text-gray-300"
+            : "bg-[#f6f7f9] text-black"
+        }`}
+      >
+        <Outlet />
+      </main>
     </>
   );
 };
